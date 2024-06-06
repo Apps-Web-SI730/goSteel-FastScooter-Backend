3c40261189236fa82fa8b8bb221b56eb3379bac5
USER <u202119880@upc.udu.pe>
2024-06-06 16:20:52 -0500

    feat:add new methods for Rent model in RentMySQLInfrastructure.cs

--- FastScooter.Infrastructure/Repositories/RentMySQLInfrastructure.cs
+++ FastScooter.Infrastructure/Repositories/RentMySQLInfrastructure.cs
@@ -18,9 +18,9 @@ public class RentMySQLInfrastructure : IRentInfrastructure
     }
     
     // Interface methods implementation
-    public async Task<List<Rent>> GetGetByScooterId(int scooterId)
+    public  List<Rent> GetByScooterIdNoAsync(int scooterId)
     {
-        return await _fastScooterContext.Rents.Where(r => r.ScooterId == scooterId).ToListAsync();
+        return  _fastScooterContext.Rents.Where(r => r.ScooterId == scooterId).ToList();
     }
     public async Task<int> CreateRentAsync(Rent rent)
     {
@@ -28,5 +28,34 @@ public class RentMySQLInfrastructure : IRentInfrastructure
         await _fastScooterContext.SaveChangesAsync();
         
         return rent.Id;
+    }
+
+    public async Task<List<Rent>> GetByUserId(int userId)
+    {
+        return await _fastScooterContext.Rents.Where(rent => rent.UserId == userId).ToListAsync();
+    }
+
+    public async Task<List<Rent>> GetByScooterId(int scooterId)
+    {
+        return await _fastScooterContext.Rents.Where(rent => rent.UserId == scooterId).ToListAsync();
+    }
+
+    public List<Rent> GetAll()
+    {
+        return _fastScooterContext.Rents.Where(x=>x.IsActive==true).ToList();
+    }
+
+    public bool delete(int id)
+    {
+        var user = _fastScooterContext.Payments.Find(id);
+        user.IsActive = false;
+        _fastScooterContext.Payments.Update(user);
+        _fastScooterContext.SaveChanges();
+        return true;
+    }
+
+    public bool ExistsById(int id)
+    {
+        return _fastScooterContext.Rents.Any(rent=>rent.ScooterId==id);
     }
 }
