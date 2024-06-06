b73ee1f3383728f5893e94027f2adb75ededbfa1
USER <u202119880@upc.udu.pe>
2024-06-06 16:20:03 -0500

    feat:add new methods for Rent model in IRentInfrastructure.cs

--- FastScooter.Infrastructure/Interfaces/IRentInfrastructure.cs
+++ FastScooter.Infrastructure/Interfaces/IRentInfrastructure.cs
@@ -5,6 +5,14 @@ namespace FastScooter.Infrastructure.Interfaces;
 public interface IRentInfrastructure
 {
     // CRUD: Read and Create
-    Task<List<Rent>> GetGetByScooterId(int scooterId); 
+    List<Rent> GetByScooterIdNoAsync(int scooterId); 
     Task<int> CreateRentAsync(Rent rent);
+    Task<List<Rent>> GetByUserId(int userId);
+    Task<List<Rent>> GetByScooterId(int scooterId);
+    List<Rent> GetAll();
+    public bool delete(int id);
+    
+    public bool ExistsById(int id);
+
+
 }
