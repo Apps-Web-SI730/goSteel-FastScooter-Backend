05884c18daa4accf9415873181e90b5d7fab25a9
USER <u202119880@upc.udu.pe>
2024-06-06 16:22:52 -0500

    feat:add new methods for Favoirte model in IFavoriteInfrastructure.cs

--- FastScooter.Infrastructure/Interfaces/IFavoriteInfrastructure.cs
+++ FastScooter.Infrastructure/Interfaces/IFavoriteInfrastructure.cs
@@ -5,5 +5,8 @@ namespace FastScooter.Infrastructure.Interfaces;
 public interface IFavoriteInfrastructure
 {
     public bool save(Favorites favorites);
-    
+    Task<bool> DeleteUserAsync(int id);
+    Task<List<Favorites>> GetByUserId(int userId);
+
+    public bool existsById(int id);
 }
