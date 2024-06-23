0e616023b4e35d598d6f008622b81f24f8715055
Josue Hidalgo <U20119880@upc.edu.pe>
2024-06-22 22:02:10 -0500

    feat:add controller about Bounded context iam

---
+++ FastScooter.Presentation/IAM/UserController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastScooter.API.IAM
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/User
+        [HttpPost("Register")]
+        public  async Task<ActionResult> RegisterAsync([FromBody] string value)
+        {
+            return CreatedAtAction("Post", new { id = 1 });
+        }
+        
+        // POST: api/User
+        [HttpPost("Login")]
+        public async Task<ActionResult>LonginAsync([FromBody] string value)
+        {
+            return CreatedAtAction("Post", new { jwt = 1 });
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
