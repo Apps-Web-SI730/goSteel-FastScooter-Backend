4e0e1caf0e5fbd507766c327a6524c30edc1dd87
Josue Hidalgo <U20119880@upc.edu.pe>
2024-06-27 10:38:20 -0500

    feat(iam):fixed error to created user

--- FastScooter.API/Program.cs
+++ FastScooter.API/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-
 using FastScooter.API.Mapper;
 using FastScooter.Domain.Domain;
 using FastScooter.Domain.Interfaces;
@@ -8,11 +7,23 @@ using FastScooter.Infrastructure.Interfaces;
 using FastScooter.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS service and define the policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost",
+        builder => builder.WithOrigins("http://localhost:5173")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 // Dependency Injection: AddScoped Infrastructure and Domain
 builder.Services.AddScoped<IUserInfrastructure, UserMySQLInfrastructure>();
 builder.Services.AddScoped<IUserDomain, UserDomain>();
@@ -26,31 +37,41 @@ builder.Services.AddScoped<IPaymentInfrastructure, PaymentMySQLInfrastructure>()
 builder.Services.AddScoped<IPaymentDomain, PaymentDomain>();
 builder.Services.AddScoped<ITokenDomain, TokenDomain>();
 builder.Services.AddScoped<IEncryptDomain, EncryptDomain>();
+
 // Dependency Injection: AddAutoMapper
 builder.Services.AddAutoMapper(
-    // typeof(ModelToResponse),
     typeof(RequestToModel)
-    );
+);
+
 // Database Connection and Dependency Injection: AddDbContext
 var connectionString = builder.Configuration.GetConnectionString("FastScooterConnectionString");
 builder.Services.AddDbContext<FastScooterContext>(
     dbContextOptions => dbContextOptions.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
-    );
+);
 
 var app = builder.Build();
+
 // Create database if not exists
 using (var scope = app.Services.CreateScope())
 using (var context = scope.ServiceProvider.GetRequiredService<FastScooterContext>())
 {
     context.Database.EnsureCreated();
 }
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
+
+// Use CORS policy
+app.UseCors("AllowLocalhost");
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
